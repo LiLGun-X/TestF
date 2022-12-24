8662492f2813b988e21bf67da4bc1d8e2c2d6e00
LiLGun-X <91993374+LiLGun-X@users.noreply.github.com>
2022-12-24 07:07:43 +0700

    Update sgx.sh

--- sgx.sh
+++ sgx.sh
@@ -4,7 +4,7 @@ echo " "
 echo " "
 echo "                   ░█▒░░█░█▒░░▄▀▀░█▒█░█▄░█░▀▄▀"|lolcat
 echo "                   ▒█▄▄░█▒█▄▄░▀▄█░▀▄█░█▒▀█░█▒█"|lolcat
-echo "                                       Developer  & Gamer"|lolcat
+echo "                            Developer & Gamer"|lolcat
 echo " "
 echo " "
 echo "                  ┏┓━━━━━┏┓━━━┏━━━┓━━━━━━━━┏━┓┏━┓"|lolcat
@@ -13,8 +13,8 @@ echo "                  ┃┃━━━┏┓┃┃━━━┃┃━┗┛┏�
 echo "                  ┃┃━┏┓┣┫┃┃━┏┓┃┃┏━┓┃┃┃┃┃┏┓┓━┏┛┗┓━"|lolcat
 echo "                  ┃┗━┛┃┃┃┃┗━┛┃┃┗┻━┃┃┗┛┃┃┃┃┃┏┛┏┓┗┓"|lolcat
 echo "                  ┗━━━┛┗┛┗━━━┛┗━━━┛┗━━┛┗┛┗┛┗━┛┗━┛"|lolcat
-echo "                   ━━━━━━━━━Developer  & Gamer━━━━━━━━━"|lolcat
+echo "                   ━━━━━━━Developer & Gamer━━━━━━━"|lolcat
 echo "                   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"|lolcat
-
-
-
+echo " "
+echo " "
+echo " "
